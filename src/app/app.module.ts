18798ce8d41522c73a4bf64c6519ff3690ee5987
Md. Firoze Hossain <firoze.hossain@leads-bd.com>
2022-12-13 09:08:51 +0600

    creating components with cli and nested components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,12 @@ import { AppComponent } from './app.component';
 import {ServerComponent} from "./server/server.component";
 import { ServersComponent } from './servers/servers.component';
 
+
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
   ],
   imports: [
     BrowserModule,
