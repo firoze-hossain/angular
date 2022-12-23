b022e073b130f233830b8d159c6c29a02490f216
Md. Firoze Hossain <firoze.hossain01@yahoo.com>
2022-12-23 13:26:25 +0600

    template and DOM with @ViewChild

--- cmp-databinding-start/src/app/cockpit/cockpit.component.ts
+++ cmp-databinding-start/src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit, EventEmitter, Output} from '@angular/core';
+import {Component, OnInit, EventEmitter, Output, ViewChild, ElementRef} from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -11,7 +11,8 @@ export class CockpitComponent implements OnInit {
 
 
   //newServerName = '';
-  newServerContent = '';
+  //newServerContent = '';
+  @ViewChild('serverContentInput',{static:true}) serverContentInput:ElementRef;
 
   constructor() {
   }
@@ -20,17 +21,17 @@ export class CockpitComponent implements OnInit {
   }
 
   onAddServer(nameInput:HTMLInputElement) {
-
+//console.log(this.serverContentInput)
     this.serverCreated.emit({
       serverName: nameInput.value,
-      serverContent: this.newServerContent
+      serverContent: this.serverContentInput.nativeElement.value
     });
   }
 
   onAddBlueprint(nameInput:HTMLInputElement) {
     this.blueprintCreated.emit({
       serverName: nameInput.value,
-      serverContent: this.newServerContent
+      serverContent: this.serverContentInput.nativeElement.value
     });
   }
 }
