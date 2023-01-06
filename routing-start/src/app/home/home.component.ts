8e59b0c8de2b1301a3bb570a4239c7cacc5802e1
Md. Firoze Hossain <firoze.hossain01@yahoo.com>
2023-01-06 15:27:51 +0600

    passing query parameters and fragments

--- routing-start/src/app/home/home.component.ts
+++ routing-start/src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Router} from "@angular/router";
 
 @Component({
@@ -8,11 +8,13 @@ import {Router} from "@angular/router";
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private router:Router) { }
+  constructor(private router: Router) {
+  }
 
   ngOnInit() {
   }
-  onLoadServers(){
-this.router.navigate(['/servers']);
+
+  onLoadServer(id: number) {
+    this.router.navigate(['/servers', id, 'edit'], {queryParams: {allowEdit: '1'},fragment:'loading'});
   }
 }
