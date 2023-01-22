7f029d8d7ed89c0d0e554852bbaf3160fc644e1b
Md. Firoze Hossain <firoze.hossain01@yahoo.com>
2023-01-22 22:08:35 +0600

    using rsjs for transforming data

--- http-01-start/src/app/app.component.ts
+++ http-01-start/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import {map} from "rxjs/operators";
 
 @Component({
   selector: 'app-root',
@@ -13,6 +14,7 @@ export class AppComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.fetchPosts();
   }
 
   onCreatePost(postData: { title: string; content: string }) {
@@ -23,9 +25,27 @@ export class AppComponent implements OnInit {
 
   onFetchPosts() {
     // Send Http request
+    this.fetchPosts();
   }
 
   onClearPosts() {
     // Send Http request
+  }
+
+  private fetchPosts() {
+    this.http.get('https://ng-complete-guide-59349-default-rtdb.firebaseio.com/posts.json')
+      .pipe(map(responseData => {
+        const postsArray = [];
+        for (const key in responseData) {
+          if (responseData.hasOwnProperty(key)) {
+            postsArray.push({...responseData[key], id: key});
+          }
+
+        }
+        return postsArray;
+      }))
+      .subscribe(posts => {
+        console.log(posts);
+      });
   }
 }
