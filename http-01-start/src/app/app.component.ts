9516cbd274fc2764e1a87f2cba74bc0170ed4692
Md. Firoze Hossain <firoze.hossain01@yahoo.com>
2023-01-22 21:52:14 +0600

    sending a post request to firebase

---
+++ http-01-start/src/app/app.component.ts
@@ -0,0 +1,31 @@
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  loadedPosts = [];
+
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit() {
+  }
+
+  onCreatePost(postData: { title: string; content: string }) {
+    this.http.post('https://ng-complete-guide-59349-default-rtdb.firebaseio.com/posts.json', postData).subscribe(responseData => {
+      console.log(responseData);
+    });
+  }
+
+  onFetchPosts() {
+    // Send Http request
+  }
+
+  onClearPosts() {
+    // Send Http request
+  }
+}
