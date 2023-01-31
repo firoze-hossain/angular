2c355f2bfdf44efc782550a2bf500059019bbbbc
Md. Firoze Hossain <firoze.hossain@leads-bd.com>
2023-01-31 10:21:23 +0600

    clean up done

--- ngrx-08-auth-finished/src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ ngrx-08-auth-finished/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,12 +1,12 @@
 import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute, Params, Router} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
 
 import {Recipe} from '../recipe.model';
-import {RecipeService} from '../recipe.service';
 import {Store} from "@ngrx/store";
 import * as fromApp from "../../store/app.reducer";
 import {map, switchMap} from "rxjs/operators";
 import * as RecipesActions from "../store/recipe.actions";
+import * as ShoppingListActions from "../../shopping-list/store/shopping-list.actions";
 
 @Component({
   selector: 'app-recipe-detail',
@@ -17,10 +17,10 @@ export class RecipeDetailComponent implements OnInit {
   recipe: Recipe;
   id: number;
 
-  constructor(private recipeService: RecipeService,
-              private route: ActivatedRoute,
-              private router: Router,
-              private store: Store<fromApp.AppState>) {
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private store: Store<fromApp.AppState>) {
   }
 
   ngOnInit() {
@@ -40,7 +40,8 @@ export class RecipeDetailComponent implements OnInit {
   }
 
   onAddToShoppingList() {
-    this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
+    // this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
+    this.store.dispatch(new ShoppingListActions.AddIngredients(this.recipe.ingredients));
   }
 
   onEditRecipe() {
