26d77a876df5c468c3bfe5b7ac151c60a2ef3a12
Md. Firoze Hossain <firoze.hossain@leads-bd.com>
2023-01-31 10:13:30 +0600

    storing recipes via effects

--- ngrx-08-auth-finished/src/app/recipes/store/recipe.effects.ts
+++ ngrx-08-auth-finished/src/app/recipes/store/recipe.effects.ts
@@ -1,9 +1,11 @@
 import {Actions, Effect, ofType} from "@ngrx/effects";
 import * as RecipesActions from "./recipe.actions";
-import {map, switchMap} from "rxjs/operators";
+import {map, switchMap, withLatestFrom} from "rxjs/operators";
 import {Recipe} from "../recipe.model";
 import {HttpClient} from "@angular/common/http";
 import {Injectable} from "@angular/core";
+import {Store} from "@ngrx/store";
+import * as fromApp from '../../store/app.reducer';
 
 @Injectable()
 export class RecipeEffects {
@@ -26,7 +28,19 @@ export class RecipeEffects {
       return new RecipesActions.SetRecipes(recipes);
     })
   );
+  @Effect({dispatch: false})
+  storeRecipes = this.actions$.pipe(ofType(RecipesActions.STORE_RECIPES),
+    withLatestFrom(this.store.select('recipes')),
+    switchMap(([actionData, recipesState]) => {
+      return this.http
+        .put(
+          'https://testdb-aacd5-default-rtdb.firebaseio.com/recipes.json',
+          recipesState.recipes
+        );
+    }));
 
-  constructor(private actions$: Actions, private http: HttpClient) {
+  constructor(private actions$: Actions,
+              private http: HttpClient,
+              private store: Store<fromApp.AppState>) {
   }
 }
