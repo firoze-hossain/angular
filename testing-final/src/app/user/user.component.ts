7abe22d2b5779180ae3deaa47f8f00f495df532d
Md. Firoze Hossain <firoze.hossain@leads-bd.com>
2023-02-02 12:03:51 +0600

    adding component and some fitting test

--- testing-final/src/app/user/user.component.ts
+++ testing-final/src/app/user/user.component.ts
@@ -1,24 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-
-import { UserService } from "./user.service";
-import { DataService } from "../shared/data.service";
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css'],
-  providers: [UserService, DataService]
+  styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
-  user: {name: string};
+export class UserComponent {
+  user: {
+    name: string;
+  };
   isLoggedIn = false;
-  data: string;
-
-  constructor(private userService: UserService, private dataService: DataService) { }
-
-  ngOnInit() {
-    this.user = this.userService.user;
-    this.dataService.getDetails().then((data: string) => this.data = data);
-  }
-
 }
