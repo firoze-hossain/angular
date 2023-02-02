d27d3058ffb294567ce760e16bf7dd7b5918c37c
Md. Firoze Hossain <firoze.hossain01@yahoo.com>
2023-02-03 00:02:46 +0600

    angular libraries

---
+++ angular-config/projects/my-button/src/lib/my-button.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-my-button',
+  template: `
+   <button><ng-content></ng-content></button>
+  `,
+  styles: [
+  ]
+})
+export class MyButtonComponent {
+
+}
