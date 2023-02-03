002a3ef02e30c8224dd2aa36770b4463f9bc50cd
Md. Firoze Hossain <firoze.hossain01@yahoo.com>
2023-02-03 15:35:06 +0600

    array and object types

--- type-script/basics.ts
+++ type-script/basics.ts
@@ -4,3 +4,21 @@ let username: string;
 username = 'Firoze';
 let instruct: boolean;
 instruct = true;
+
+let hobbies: string[];
+hobbies = ["Coding", "Cricket"];
+let person: {
+    name: string,
+    age: number
+};
+person = {
+    name: "Firoze",
+    age: 32
+}
+// person = {
+//     isEmployee: true
+// }
+let pepole: {
+    name: string,
+    age: number
+}[];
