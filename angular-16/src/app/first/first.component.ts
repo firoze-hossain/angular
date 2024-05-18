63aaefb68bc3fb2d07107b5e3e4e7a80847fe48c
Md. Firoze Hossain <firoze.hossain@outlook.com>
2024-05-18 11:23:07 +0600

    ngFor directive used for show data in table when submit

--- angular-16/src/app/first/first.component.ts
+++ angular-16/src/app/first/first.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-first',
@@ -6,12 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./first.component.scss']
 })
 export class FirstComponent {
-name:string='';
-email:string='';
-message:string='';
-isSubmitted:boolean= false;
+  name: string = '';
+  email: string = '';
+  message: string = '';
+  isSubmitted: boolean = false;
+  messages: Array<any> = [];
 
-  onSubmit() :void{
-    console.log(this.isSubmitted=true);
+  onSubmit(): void {
+    // console.log(this.isSubmitted = true);
+    this.isSubmitted = true;
+    this.messages.push({
+      "name": this.name,
+      "email": this.email,
+      "message": this.message
+    });
+    console.log(this.messages);
   }
 }
