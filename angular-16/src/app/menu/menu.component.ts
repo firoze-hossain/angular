22d7273dc58e2d294412832b1dc954cdd8c78daa
Md. Firoze Hossain <firoze.hossain@outlook.com>
2024-05-19 21:14:34 +0600

    navigate page using router service

--- angular-16/src/app/menu/menu.component.ts
+++ angular-16/src/app/menu/menu.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-menu',
@@ -6,8 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./menu.component.scss']
 })
 export class MenuComponent {
+  constructor(private router: Router) {
+  }
 
   navigateToAbout() {
-
+    this.router.navigate(['about']);
   }
 }
